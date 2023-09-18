16b35fd0ed6d4788c04e272c120813028c48708b
parvinkhm <[parvinkhm]>
2023-09-18 23:07:47 +0400

    added new file top menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -119,20 +119,39 @@
 // }
 
 
+// let tabHeaders = document.querySelectorAll(".tab-header .item");
+// let tabContents = document.querySelectorAll(".tab-content .item");
+
+// tabHeaders.forEach(tabHeader => {
+//     tabHeader.addEventListener("click", function () {
+//         document.querySelector(".active-tab").classList.remove("active-tab");
+//         this.classList.add("active-tab")
+
+//         tabContents.forEach(content => {
+//             if (content.getAttribute("data-id") == this.getAttribute("data-id")) {
+//                 content.classList.remove("d-none")
+//             } else {
+//                 content.classList.add("d-none")
+//             }
+//         });
+//     })
+// });
+
+
 let tabHeaders = document.querySelectorAll(".tab-header .item");
 let tabContents = document.querySelectorAll(".tab-content .item");
 
 tabHeaders.forEach(tabHeader => {
-    tabHeader.addEventListener("click", function(){
+    tabHeader.addEventListener("click", function () {
         document.querySelector(".active-tab").classList.remove("active-tab");
-         this.classList.add("active-tab")
+        this.classList.add("active-tab")
 
-         tabContents.forEach(content => {
-             if(content.getAttribute("data-id") == this.getAttribute("data-id")){
-                 content.classList.remove("d-none")
-             }else{
-                 content.classList.add("d-none")
-             }
-         });
+        tabContents.forEach(content => {
+            if (content.getAttribute("data-id") == this.getAttribute("data-id")) {
+                content.classList.remove("d-none")
+            } else {
+                content.classList.add("d-none")
+            }
+        });
     })
 });
